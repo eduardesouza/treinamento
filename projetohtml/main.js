f84ddd54e25b5fa2f0076ea9de5453b1cdf095d8
Joubert <joubert.bh6@gmail.com>
2022-03-10 13:26:00 -0300

    Criar interatividade
    Adicionei o js

---
+++ projetohtml/main.js
@@ -0,0 +1,9 @@
+function bloco1(){
+    document.getElementById('divPrimeira').style.display = 'inline'
+    document.getElementById('divSegunda').style.display = 'none'
+}
+
+function bloco2(){
+    document.getElementById('divPrimeira').style.display = 'none'
+    document.getElementById('divSegunda').style.display = 'inline'
+}
